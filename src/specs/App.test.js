dc1ccf7d208d3f080e38b0a55de0094a5c45b6b0
Rene Fernando Lins Pereira de Souza <rene.souza@getnet.com.br>
2020-02-26 23:55:19 -0300

    [react-test] - terminando campitulo inicial

--- src/specs/App.test.js
+++ src/specs/App.test.js
@@ -3,7 +3,12 @@ import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import App from '../app_index';
+
+const path = process.env.COMPONENTS_PATH;
+
 Enzyme.configure({ adapter: new EnzymeAdapter()});
+
+
 
 /**
  * Factory function to create shallow wrapper for App component
@@ -53,41 +58,43 @@ it('Renders increment button', () => {
    expect(h1.length).toBe(1);
  });   
 
- it('counter start at 0', () => {
-   const wrapper = setup();
-   const initialCounter = wrapper.state('counter');
-   expect(initialCounter).toBe(0);
- });  
- 
- it('click button increments the counter', () => {
-   const initialCounter = 2;
-   const wrapper = setup(null, {counter: initialCounter});
+ if(path === './src/with-class'){
+   it('counter start at 0', () => {
+      const wrapper = setup();
+      const initialCounter = wrapper.state('counter');
+      expect(initialCounter).toBe(0);
+   });  
+   
+   it('click button increments the counter', () => {
+      const initialCounter = 2;
+      const wrapper = setup(null, {counter: initialCounter});
 
-   const button = findDataTestAttr(wrapper, 'increment-button');
-   button.simulate('click');
+      const button = findDataTestAttr(wrapper, 'increment-button');
+      button.simulate('click');
 
-   const h1 =  findDataTestAttr(wrapper,'counter-display');
-   expect(h1.text()).toContain(initialCounter + 1)
- });  
+      const h1 =  findDataTestAttr(wrapper,'counter-display');
+      expect(h1.text()).toContain(initialCounter + 1)
+   });  
 
- it('click button decrements the counter', () => {
-   const initialCounter = 1;
-   const wrapper = setup(null, {counter: initialCounter});
+   it('click button decrements the counter', () => {
+      const initialCounter = 1;
+      const wrapper = setup(null, {counter: initialCounter});
 
-   const button = findDataTestAttr(wrapper, 'decrement-button');
-   button.simulate('click');
+      const button = findDataTestAttr(wrapper, 'decrement-button');
+      button.simulate('click');
 
-   const h1 =  findDataTestAttr(wrapper,'counter-display');
-   expect(h1.text()).toContain(initialCounter - 1)
- });  
+      const h1 =  findDataTestAttr(wrapper,'counter-display');
+      expect(h1.text()).toContain(initialCounter - 1)
+   });  
 
- it('click button show message tha counter can\'t be lesser than 0', () => {
-   const initialCounter = 0;
-   const wrapper = setup(null, {counter: initialCounter});
+   it('click button show message tha counter can\'t be lesser than 0', () => {
+      const initialCounter = 0;
+      const wrapper = setup(null, {counter: initialCounter});
 
-   const button = findDataTestAttr(wrapper, 'decrement-button');
-   button.simulate('click');
+      const button = findDataTestAttr(wrapper, 'decrement-button');
+      button.simulate('click');
 
-   const h1 =  findDataTestAttr(wrapper,'counter-display');
-   expect(h1.text()).toBe('Contador não pode ser menor do que 0');
- });  
+      const h1 =  findDataTestAttr(wrapper,'counter-display');
+      expect(h1.text()).toBe('Contador não pode ser menor do que 0');
+   });  
+}
--- src/with-hooks/App.js
+++ src/with-hooks/App.js
@@ -6,6 +6,7 @@ const App = props => {
   return (
     <div className="counter-app" data-test="app-component">
       <h1 data-test="counter-display">O contador está em {/*counterState*/}</h1>
+      <button data-test="decrement-button" onClick={()=>{/*setCounterState(counterState + 1)*/}}>Remover do contador</button>
       <button data-test="increment-button" onClick={()=>{/*setCounterState(counterState + 1)*/}}>Adicionar ao contador</button>
     </div>
   );
