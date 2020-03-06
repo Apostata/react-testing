03a6dd70d08ef55c3705b3fe14c58e7dbf2f69b9
Rene Fernando Lins Pereira de Souza <rene.souza@getnet.com.br>
2020-03-06 11:57:03 -0300

    [react-test] - Testing success reducer

--- src/store/reducers/success/success.reducer.js
+++ src/store/reducers/success/success.reducer.js
@@ -1,7 +1,16 @@
+import { actionTypes } from '../../actionTypes'
 /**
  * @function successReducer
  * @param {array} state - Array of guessed words
  * @param {object} action - Action to be reduced
  * @returns {boolean} - new success state
  */
-export default (state, action) => null;
+const initialState = false;
+export default (state=initialState, action) =>{
+    switch(action.type){
+        case actionTypes.CORRECT_GUESS:
+            return true
+        default:
+            return state;
+    }
+};
