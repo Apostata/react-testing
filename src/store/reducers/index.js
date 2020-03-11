29b0bba20a2f22bac43bb5d397896710067e2ae3
Rene Fernando Lins Pereira de Souza <rene.souza@getnet.com.br>
2020-03-11 17:56:28 -0300

    [react-test] - Testando redux-thunk

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import successReducer from './success/success.reducer';
+import guessedWordsReducer from './guessWord/guessWord.reducer';
+import secretWordReducer from './secretWord/secretWord.reducer';
 
 export default combineReducers({
     success: successReducer,
-})
+    guessedWords: guessedWordsReducer,
+    secretWord: secretWordReducer
+});
